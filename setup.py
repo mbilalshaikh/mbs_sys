2ed07c304e00a341d6159632a871ec665881e975
Bilal Shaikh <mbilalshaikh@users.noreply.github.com>
2021-05-19 16:34:45 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
   download_url = 'https://github.com/mbilalshaikh/mbs_sys/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['system-info', 'ip-address', 'mac-address'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
